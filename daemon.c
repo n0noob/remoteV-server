73ba2d64df895031d894c8b808b715239f8779eb
n0noob <neemanoop@gmail.com>
2015-12-25 23:56:39 +0530

    states defined

--- daemon.c
+++ daemon.c
@@ -10,52 +10,69 @@
 
 int main(void) {
         
-        /* Our process ID and Session ID */
-        pid_t pid, sid;
-        
-        /* Fork off the parent process */
-        pid = fork();
-        if (pid < 0) {
-                exit(EXIT_FAILURE);
-        }
-        /* If we got a good PID, then
-           we can exit the parent process. */
-        if (pid > 0) {
-                exit(EXIT_SUCCESS);
-        }
+    /* Our process ID and Session ID */
+    pid_t pid, sid;
+    
+    enum states{
+        SEARCH,
+        PROMPT,
+        CONNECTED
+    }STATE;
+    STATE = SEARCH;
 
-        /* Change the file mode mask */
-        umask(0);
-                
-        /* Open any logs here */        
-                
-        /* Create a new SID for the child process */
-        sid = setsid();
-        if (sid < 0) {
-                /* Log the failure */
-                exit(EXIT_FAILURE);
-        }
-        
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    /* If we got a good PID, then
+    we can exit the parent process. */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
 
+    /* Change the file mode mask */
+    umask(0);
+    
+    /* Open any logs here */
+
+    /* Create a new SID for the child process */
+    sid = setsid();
+    if (sid < 0) {
+        /* Log the failure */
+        exit(EXIT_FAILURE);
+    }
         
-        /* Change the current working directory */
-        if ((chdir("/")) < 0) {
-                /* Log the failure */
+        
+    /* Change the current working directory */
+    if ((chdir("/")) < 0) {
+        /* Log the failure */
+        exit(EXIT_FAILURE);
+    }
+        
+    /* Close out the standard file descriptors */
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+        
+    /* Daemon-specific initialization goes here */
+    
+    /* The Big Loop */
+    while (1) {
+        /* Do some task here ... */
+        
+        switch(STATE){
+            case SEARCH:
+                break;
+            case PROMPT:
+                break;
+            case CONNECTED:
+                break;
+            default:
                 exit(EXIT_FAILURE);
-        }
-        
-        /* Close out the standard file descriptors */
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
-        
-        /* Daemon-specific initialization goes here */
-        
-        /* The Big Loop */
-        while (1) {
-           /* Do some task here ... */
-           
-           sleep(30); /* wait 30 seconds */
-        }
-   exit(EXIT_SUCCESS);
+        } 
+
+        sleep(3); /* wait 30 seconds */
+    }
+    exit(EXIT_SUCCESS);
 }
