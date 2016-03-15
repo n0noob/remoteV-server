5ec9df259f5329b37651592c648a0c5fd220f11d
n0noob <neemanoop@gmail.com>
2016-03-16 00:45:49 +0530

    new file added

--- tcp_server.c
+++ tcp_server.c
@@ -10,10 +10,11 @@
 #include <time.h> 
 #include <limits.h>
 #include <signal.h>
+
 #include "tcp_server.h"
+#include "mpv_func.h"
 
 #define TEST_FILE "./mediafile.list"
-#define MPV_SOCKET_FILE "/tmp/mpv_socket"
 #define D_BUG
 
 
@@ -22,21 +23,6 @@ COMMAND cmd[] = {{"$LIST", 1}, {"$PLAY", 2}, {"$STOP", 3}, {"$PAUS", 4}, {"$FFWD
 const int cmd_count = 6;
 const int cmd_len = 5;
 
-
-int touch(char *fpath){
-
-    if(access(fpath, F_OK|R_OK) != -1){
-        return 0;
-    }
-
-    FILE *fp; 
-    if((fp = fopen(fpath, "w")) == NULL){
-        perror("Error: ");
-        exit(EXIT_FAILURE);
-    }
-    fclose(fp);
-    return 0;
-}
 
 int compare_string(char * a, char * b)
 {
@@ -104,94 +90,6 @@ char * extract_path(char *buffer, int val)
 }
 */
 
-int mpv_setup(MPV_INSTANCE * instance)
-{
-    pid_t pid;
-
-    if(instance->instance_count > 0){
-        return -1;
-    }
-
-    if((pid = fork()) < 0){
-        printf("Error in forking mpv!");
-        perror("Error: ");
-        exit(2);
-    }
-    else if(pid == 0){          //for child process
-        touch(MPV_SOCKET_FILE);
-        execl("/bin/mpv", "/bin/mpv", "--profile=pseudo-gui", "--input-unix-socket="MPV_SOCKET_FILE, NULL);
-        exit(EXIT_SUCCESS); 
-    }
-    else{
-        instance->mpv_pid = pid;
-        //Still need to check this code
-        instance->instance_count = instance->instance_count + 1;
-        return 0;
-    }
-}
-
-int mpv_play(char *file)
-{
-    char command[2048];
-    memset(command, 0, sizeof(command));
-    snprintf(command, 2048, "echo \'{ \"command\": [\"loadfile\", \"%s\"] }\' | socat - /tmp/mpv_socket", file);
-
-    system(command);
-    return 0;
-}
-
-int mpv_stop(MPV_INSTANCE * instance)
-{
-    char command[2048];
-
-    if(instance->instance_count == 0){
-        printf("Error : Player not running");
-        return -1;
-    }
-
-    memset(command, 0, sizeof(command));
-    snprintf(command, 2048, "echo \'{ \"command\": [\"stop\"] }\' | socat - /tmp/mpv_socket");
-
-    system(command);
-    instance->instance_count = 0;
-    instance->mpv_pid = 0;
-
-    return 0;
-}
-
-int mpv_pause(MPV_INSTANCE * instance)
-{
-    char command[2048];
-
-    if(instance->instance_count == 0){
-        printf("Error : Player not running");
-        return -1;
-    }
-
-    memset(command, 0, sizeof(command));
-    snprintf(command, 2048, "echo \'{ \"command\": [\"cycle\", \"pause\"] }\' | socat - /tmp/mpv_socket");
-
-    system(command);
-
-    return 0;
-}
-
-int mpv_seek(MPV_INSTANCE * instance, int seconds)
-{
-    char command[2048];
-
-    if(instance->instance_count == 0){
-        printf("Error : Player not running");
-        return -1;
-    }
-
-    memset(command, 0, sizeof(command));
-    snprintf(command, 2048, "echo \'{ \"command\": [\"seek\", \"%d\"] }\' | socat - /tmp/mpv_socket", seconds);
-
-    system(command);
-
-    return 0;
-}
 
 int main(int argc, char *argv[])
 {
